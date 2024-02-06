ca0ef0b665d6214610ec688cf3fbd5df3b257249
Karen Nowo <nowo.karen@gmail.com>
2024-02-06 17:32:41 -0300

    Added Pylint and nbqa and fix style

--- lcanalyzer/models.py
+++ lcanalyzer/models.py
@@ -3,13 +3,13 @@
 The Model layer is responsible for the 'business logic' part of the software.
 
 The lightcurves are saved in a table (2D array) where each row corresponds to a single observation. 
-Depending on the dataset (LSST or Kepler), a table can contain observations of a single or several objects, 
+Depending on the dataset (LSST or Kepler), a table 
+can contain observations of a single or several objects, 
 in a single or different bands.
 """
 
 import pandas as pd
 import numpy as np
-from astropy.timeseries import LombScargle
 
 def load_dataset(filename):
     """Load a table from CSV file.
@@ -21,12 +21,20 @@ def load_dataset(filename):
 
 
 def mean_mag(data,mag_col):
-    """Calculate the mean magnitude of a lightcurve"""
-    return data[mag_col].mean()
+    """Calculate the mean magnitude of a lightcurve
+    :param data: pd.DataFrame with observed magnitudes for a single source.
+    :param mag_col: a string with the name of the column for calculating the mean.
+    :returns: A float with mean value of the column
+    """
+    return np.mean(data[mag_col], axis=0)
 
 
 def max_mag(data,mag_col):
-    """Calculate the max magnitude of a lightcurve"""
+    """Calculate the max magnitude of a lightcurve
+    :param data: pd.DataFrame with observed magnitudes for a single source.
+    :param mag_col: a string with the name of the column for calculating the max value.
+    :returns: The max value of the column.
+    """
     return data[mag_col].max()
 
 
